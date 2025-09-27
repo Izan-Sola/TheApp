fde8c8e2bfa6624ce7c7c2f49228e81f8829b419
Izan Sola Montesinos <83542827+Izan-Sola@users.noreply.github.com>
2025-09-27 19:45:37 +0200

    Add files via upload

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,6 +1,8 @@
 module me.shinyshadow_.apptest.demo {
     requires javafx.controls;
     requires javafx.fxml;
+    requires jdk.xml.dom;
+    requires java.desktop;
 
 
     opens me.shinyshadow_.apptest.demo to javafx.fxml;
